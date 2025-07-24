21095b441aac8f2802d458abbe953cff08f01196
zywilliamli <twelvehertz@gmail.com>
2025-07-24 13:02:39 +1000

    update

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ def train():
 
     cfg = SFTConfig(
         output_dir="PG_smollm",
-        num_train_epochs=1,
+        num_train_epochs=2,
         per_device_train_batch_size=4,
-        gradient_accumulation_steps=4,
-        learning_rate=1e-5,
+        gradient_accumulation_steps=1,
+        learning_rate=2e-5,
         warmup_ratio=0.03,
         lr_scheduler_type="cosine",
         logging_steps=1,
