228e04ea2cb965cecdd82d77ba42c6c11ed253e8
zywilliamli <twelvehertz@gmail.com>
2025-07-24 12:15:07 +1000

    update

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def train():
         num_train_epochs=2,
         per_device_train_batch_size=4,
         gradient_accumulation_steps=8,
-        learning_rate=5e-5,
+        learning_rate=2e-5,
         warmup_ratio=0.03,
         lr_scheduler_type="cosine",
         logging_steps=1,
