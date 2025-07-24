592543fe8f04f3a0a7fe5e6defa21bd0a67c45c7
zywilliamli <twelvehertz@gmail.com>
2025-07-24 13:28:23 +1000

    update

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ def train():
     trainer.train()
     trainer.save_model(cfg.output_dir)
     tokenizer.save_pretrained(cfg.output_dir)
+    model.config.save_pretrained(cfg.output_dir)
 
     pipe = pipeline(
         "text-generation",
