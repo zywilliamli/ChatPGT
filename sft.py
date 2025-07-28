cfc67e76fd7131d7d3f5003c21e8c7561de8d187
zywilliamli <twelvehertz@gmail.com>
2025-07-28 14:49:21 +1000

    update params

--- sft.py
+++ sft.py
@@ -52,8 +52,8 @@ def train():
     cfg = SFTConfig(
         output_dir="SmolGraham",
         num_train_epochs=2,
-        per_device_train_batch_size=8,
-        gradient_accumulation_steps=2,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
         learning_rate=1e-5,
         warmup_ratio=0.03,
         lr_scheduler_type="cosine",
